6006887f965dba4fbfb0d62194ec10d10a302078
Cristian Romo <caromo.33@outlook.com>
2017-12-14 15:34:15 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  Emails = [
+	  {email: 'eatass69@yahoo.com', importance: true, subject:'Human Resources', content:'Tone it down on please'},
+	  {email: 'amherst13@aol.com', importance: false, subject: 'the block', content:'the block needs you'},
+	  {email: 'harberd@harderd.edu', importance: true, subject:'Application', content:'We regret to inform you'},
+	  {email: 'Devry@DevryUni.edu', importance: false, subject: 'Application', content:'Congrajulations'}
+  ]
 }
